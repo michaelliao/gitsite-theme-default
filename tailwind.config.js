943ea8285fc1614b45e1c8992acf2e2b263f5f38
Michael Liao <askxuefeng@gmail.com>
2024-01-12 20:22:37 +0800

    add theme source

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'class',
+    content: ['./**/*.html'],
+    theme: {
+        extend: {},
+    },
+    plugins: ['forms', 'typography', 'aspect-ratio'],
+}
