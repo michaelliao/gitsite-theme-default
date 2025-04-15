430987fd5fd74458be6f8aa948df3f8a4bdef514
Michael Liao <askxuefeng@gmail.com>
2025-04-15 08:37:19 +0800

    update post-build.mjs

--- post-build.mjs
+++ post-build.mjs
@@ -9,5 +9,5 @@ export default async function (config, output) {
     console.log(`post-build: cwd = ${process.cwd()}, output = ${output}`);
     run(`cp -r static ${output}/`);
     run(`npm install -D tailwindcss@3`);
-    run(`npx tailwindcss -c tailwind.config.js -i default-tailwind.tcss -o ${output}/static/default-tailwind.css`);
+    run(`npx tailwindcss@3 -c tailwind.config.js -i default-tailwind.tcss -o ${output}/static/default-tailwind.css`);
 };
